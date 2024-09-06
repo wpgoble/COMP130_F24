28b6aca9e3126725cc74e1b301d7c25f9e118a8d
wpgoble <will.goble@gmail.com>
2024-09-06 19:41:06 -0400

    adds files from the first week of class

---
+++ week1/helloWorld.py
@@ -0,0 +1,5 @@
+phrase = "Hello World"
+print("Hello World")
+print(phrase)
+info = input("What is your favorite number?")
+print(info, type(info))
---
+++ week1/lab01.py
@@ -0,0 +1,8 @@
+####################################
+# Team Member 1: Jane Doe
+# Team Member 2: John Deere
+# Lab 1
+####################################
+# Q5
+# print("My name is William ...")
+# print("My name is Fox ...")
