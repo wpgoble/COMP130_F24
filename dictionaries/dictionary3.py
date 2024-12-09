922ca42dd4ef497805c762d57a51e0d8474183cd
wpgoble <will.goble@gmail.com>
2024-12-09 09:18:36 -0500

    adds additional examples

--- dictionaries/dictionary3.py
+++ dictionaries/dictionary3.py
@@ -5,6 +5,10 @@ sports = ["baseball", "football", "basketball"]
 # Example 2
 philly = {"baseball":"Phillies", "football":"Eagles", "basketball":"76ers"}
 
+# How would we draw a state diagram for example 1?
+
+# how would we draw a state diagram for example 2?
+
 # How would we add the pair "soccer" and "Union" to example 1?
 
 # How would we add the pair "soccer" and "Union" to example 2?
